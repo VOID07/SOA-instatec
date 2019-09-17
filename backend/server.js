fac33c16c972903ce7108409fc287c7a5e5de957
Dagoberto Keylor Rojas Juarez <krojas96ce@gmail.com>
2019-09-17 08:49:08 -0600

    Add initial code base

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const express = require ('express')
+const app  =  express()
+const port = 3000
+app.get('/hello-world', (requ, res) => res.send('hello my madafakas'));
+
+app.listen(port, () => console.log(`listening on port ${port}`));
